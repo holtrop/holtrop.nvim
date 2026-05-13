882286f6bb1ff76c9cc50d664f782621a1070595
Josh Holtrop <jholtrop@gmail.com>
2026-05-13 13:59:07 -0400

    Add fzf-lua

--- plugin/holtrop.lua
+++ plugin/holtrop.lua
@@ -1,2 +1,10 @@
+-- Load tokyonight color scheme plugin
 vim.opt.rtp:prepend("~/.config/nvim/holtrop.nvim/tokyonight.nvim")
 vim.cmd([[colorscheme tokyonight-night]])
+
+-- Load fzf-lua plugin
+vim.opt.rtp:prepend("~/.config/nvim/holtrop.nvim/fzf-lua.nvim")
+vim.cmd([[runtime plugin/fzf-lua.lua]])
+
+-- Show titlestring in GUI window title
+vim.opt.title = true
